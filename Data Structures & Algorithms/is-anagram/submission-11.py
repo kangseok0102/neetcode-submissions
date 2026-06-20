06900356e47ba605f2c19a32db2949de3ff28183
Kangseok Lee <58051320+kangseok0102@users.noreply.github.com>
2026-06-20 17:19:46 -0400

    Add: is-anagram - submission-11

---
+++ Data Structures & Algorithms/is-anagram/submission-11.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        seen = {}
+
+        for char in s:
+            seen[char] = seen.get(char, 0) + 1
+        
+        for char in t:
+            if char in seen:
+                seen[char] -= 1
+        
+        for key in seen:
+            if seen[key] != 0:
+                return False
+        
+        return True
