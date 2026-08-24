dd930c9c50c266c36e867726a8b93f2405cf228b
Kangseok Lee <58051320+kangseok0102@users.noreply.github.com>
2026-08-24 13:57:13 -0400

    Add: two-integer-sum-ii - submission-9

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-9.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        seen = {}
+
+        for num in range(len(numbers)):
+            remain = target - numbers[num]
+
+            if remain in seen:
+                return [seen[remain] + 1, num + 1]
+            
+            seen[numbers[num]] = num
